13c7a909b9eb49684bb47469562af2d911008ea4
ErtiDesu <erti.rakipaj@gmail.com>
2023-06-04 15:17:51 +0200

    Initial Commit, neccessary project files

---
+++ src/main/java/com/laertrakipaj/Monthly/Expenses/Calculator/API/MonthlyExpensesCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.laertrakipaj.Monthly.Expenses.Calculator.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlyExpensesCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlyExpensesCalculatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laertrakipaj/Monthly/Expenses/Calculator/API/MonthlyExpensesCalculatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laertrakipaj.Monthly.Expenses.Calculator.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlyExpensesCalculatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
