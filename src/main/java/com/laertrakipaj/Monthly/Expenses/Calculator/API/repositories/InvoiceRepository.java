31a9cb256b36b727b154581d9de1d4aa3c8bea05
ErtiDesu <erti.rakipaj@gmail.com>
2023-06-08 09:54:42 +0200

    Added some neccessary functions on repo interfaces, changed the Item model class

--- src/main/java/com/laertrakipaj/Monthly/Expenses/Calculator/API/repositories/InvoiceRepository.java
+++ src/main/java/com/laertrakipaj/Monthly/Expenses/Calculator/API/repositories/InvoiceRepository.java
@@ -4,5 +4,5 @@ import com.laertrakipaj.Monthly.Expenses.Calculator.API.model.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface InvoiceRepository extends JpaRepository<Invoice,Long> {
-
+    void deleteById(Long id);
 }
